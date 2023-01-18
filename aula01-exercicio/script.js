bcec47c42b56eccfb44b64b76d659e632c060df2
Guil_NtC <guilberts48@gmail.com>
2023-01-18 16:50:16 -0300

    Exercício arrays, objects, functions

---
+++ aula01-exercicio/script.js
@@ -0,0 +1,29 @@
+
+function executar () {
+    const form = document.querySelector('.form')
+    const resultado = document.querySelector('.resultado')
+    const pessoas = []
+
+    function recebeEventoForm (evento){
+        evento.preventDefault()
+        const nome = form.querySelector('.nome')
+        const sobrenome = form.querySelector('.sobrenome')
+        const peso = form.querySelector('.peso')
+        const altura = form.querySelector('.altura')
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        })
+
+        resultado.innerHTML += `<h1>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}<h1> `
+        
+
+        
+    }
+
+    form.addEventListener('submit', recebeEventoForm)
+
+};
+executar()
