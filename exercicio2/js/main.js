c3a6ba31d02026be85de81ac457b03e8be474f1b
Guil_NtC <guilberts48@gmail.com>
2023-01-31 19:51:33 -0300

    Melhoria no código

--- exercicio2/js/main.js
+++ exercicio2/js/main.js
@@ -2,6 +2,8 @@
 function global() {
 const section = document.querySelector('.countainer')
 const resultado = document.querySelector('.resultado')
+
+
 function calcular(evento){
    evento.preventDefault()
    let alt = document.querySelector('.altura')
@@ -11,21 +13,38 @@ function calcular(evento){
    let res = ((pesoValue) / alturaValue ** 2).toFixed(1)
    let diagnostico = ''
 
-   if(res < 18.5){
-      diagnostico = 'Você está abaixo do peso'
-   } else if( res >= 18.5 && res <= 24.9){
-      diagnostico = 'Você está no peso normal'
-   } else if( res >= 25 && res <= 29.9){
-      diagnostico = 'Você está com um Sobrepeso'
-   } else if( res >= 30 && res <= 34.9){
-      diagnostico = 'Você está com obesidade grau 1'
-   } else if( res >= 35 && res <= 39.9){
-      diagnostico = 'Você está com obesidade grau 2'
-   } else if( res > 40){
-      diagnostico = 'Você está com obesidade grau 3'
+   if(!pesoValue){
+      alert('Peso inválido, digite novamente')
+      return
+   }
+   if(!alturaValue){
+      alert('Altura inválida, digite novamente')
+      return
+   }
+   const nivel = [
+      'Abaixo do peso',
+      'Peso normal',
+      'Sobrepeso',
+      'Obesidade grau 1',
+      'Obesidade grau 2',
+      'Obesidade grau 3'
+   ]
+
+   if (res >= 39.9){
+      diagnostico = nivel[5]
+   } else if( res >= 34.9){
+      diagnostico = nivel[4]
+   } else if( res >= 29.9){
+      diagnostico = nivel[3]
+   } else if( res >= 24.9){
+      diagnostico = nivel[2]
+   } else if( res >= 18.5){
+      diagnostico = nivel[1]
+   } else if( res < 18.5){
+      diagnostico = nivel[0]
    }
 
-   resultado.innerHTML = `Seu resultado é: ${res}, ${diagnostico}`
+   resultado.innerHTML = `Seu resultado é: ${res} (${diagnostico})`
 }
 
 
