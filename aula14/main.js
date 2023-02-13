66b865956ea1fe821cc030e3c4fe9c1330b609fd
Guil_NtC <guilberts48@gmail.com>
2023-02-13 15:43:25 -0300

    Aula 14(Herança em classes)

--- aula14/main.js
+++ aula14/main.js
@@ -1,16 +1,35 @@
-//Getters e Setters em class
-
-class Carro{
+//Herança nas classes
+class DispositivoEletronico{
     constructor(nome){
         this.nome = nome
-        this.velocidade = 0
-    }
-    acelerar(){
-        if(this.velocidade >= 100) return
-        this.velocidade ++
+        this.ligado = false
     }
 
-    freiar(){
-        
+    ligar(){
+        if(this.ligado) {
+            console.log('Dispositivo já ligado')
+            return
+        } 
+        this.ligado = true
+        console.log(`${this.nome} já ligadob`)
+    }
+    desligar(){
+        if(!this.ligado){
+            console.log(`${this.nome} desligado`)
+        }
+        this.ligado = false
+    }
+
+}
+
+class Smartphone extends DispositivoEletronico{
+    constructor(nome, cor, modelo){
+        super(nome)
+        this.cor = cor
+        this.modelo = modelo
     }
 }
+
+const d1 = new Smartphone('Celular', 'preto', '13 pro')
+d1.ligar()
+console.log(d1)
