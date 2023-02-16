22bbb36aad2b9f40184690794a2572ea0756441e
Guil_NtC <guilberts48@gmail.com>
2023-02-16 17:14:13 -0300

    Aula 01 (Modules)

---
+++ node/aula01/app.js
@@ -0,0 +1,6 @@
+const path = require('path')
+const axios = require('axios')
+const { Pessoa } = require('./mode1') 
+const mode1 = require('./mode1')
+
+console.log(mode1)
---
+++ node/aula01/mode1.js
@@ -0,0 +1,11 @@
+class Pessoa {
+    constructor(nome){
+        this.nome = nome
+    }
+}
+const nome = 'guill'
+const sobrenome = 'silva'
+
+module.exports = {
+    nome, sobrenome, Pessoa
+}
