4bcfa35595214acd0e8b1c7c14853f5bd952af19
Guil_NtC <guilberts48@gmail.com>
2023-02-21 11:31:05 -0300

    Aula 08(MODELO DE PROJETO)

---
+++ node/aula08/server.js
@@ -0,0 +1,18 @@
+//utilizando o express
+const express = require('express')
+const app = express()
+const routes = require('./routes')
+const path = require('path')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+app.set('views', path.resolve(__dirname, 'src', 'views'))
+app.set('view engine','ejs')
+
+app.use(routes)
+
+app.listen(3000, () =>{
+    console.log('Acessar http://localhost:3000')
+    console.log('servidor executando na porta 3000')
+})
